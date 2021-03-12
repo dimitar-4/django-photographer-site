c38eadd688b9766dcea7ef20158b531d821ad7a4
dimitar-4 <dcholakov4@gmail.com>
2021-03-12 18:08:59 +0000

    Update context processor

--- bag/contexts.py
+++ bag/contexts.py
@@ -1,6 +1,8 @@
 # This code is copied from Boutique Ado project
 from decimal import Decimal
 from django.conf import settings
+from django.shortcuts import get_object_or_404
+from products.models import Product
 
 
 def bag_contents(request):
@@ -8,8 +10,19 @@ def bag_contents(request):
     bag_items = []
     total = 0
     product_count = 0
+    bag = request.session.get('bag', {})
 
-    if total == settings.STANDART_DELIVERY_PERCENTAGE:
+    for item_id, quantity in bag.items():
+        product = get_object_or_404(Product, pk=item_id)
+        total += quantity * product.price
+        product_count += quantity
+        bag_items.append({
+            'item_id': item_id,
+            'quantity': quantity,
+            'product': product,
+        })
+
+    if total <= settings.STANDART_DELIVERY_PERCENTAGE:
         delivery = total * Decimal(settings.STANDARD_DELIVERY_PERCENTAGE / 100)
     else:
         delivery = 0
