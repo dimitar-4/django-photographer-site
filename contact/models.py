ea9c69f1a5299922ecfb03ce80420d579839cc6c
dimitar-4 <dcholakov4@gmail.com>
2021-03-20 22:29:00 +0000

    Add contact model and admin. Complete views and form to send messages to the admin

--- contact/models.py
+++ contact/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Contact(models.Model):
+    firstname = models.CharField(max_length=50)
+    lastname = models.CharField(max_length=50)
+    email = models.EmailField()
+    message = models.TextField(max_length=400)
+
+    def __str__(self):
+        return f'{self.firstname} {self.lastname}'
--- contact/views.py
+++ contact/views.py
@@ -1,9 +1,25 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import Contact
+from django.contrib import messages
 
 # Create your views here.
 
 
 def contact(request):
-    """ A view to return the about page """
+    """ A view to send messages to the admin """
+    if request.method == 'POST':
+        contact = Contact()
+        firstname = request.POST.get('firstname')
+        lastname = request.POST.get('lastname')
+        email = request.POST.get('email')
+        message = request.POST.get('message')
+        contact.firstname = firstname
+        contact.lastname = lastname
+        contact.email = email
+        contact.message = message
+        contact.save()
+        messages.success(request, 'Thank you for your message! \
+        We will get in touch as soon as possible.')
+        return redirect('home')
 
     return render(request, 'contact/contact.html')
