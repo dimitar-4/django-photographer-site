24e92ab3fa06dbe050035c7234e31ac61bf202d0
dimitar-4 <dcholakov4@gmail.com>
2021-03-21 18:02:02 +0000

    Remove secret key and set debug

--- dec_photography/settings.py
+++ dec_photography/settings.py
@@ -22,10 +22,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '*5al*9n$hc0&nz+&et^@681racqq9zwsg#$es49v6d=djwp8xx'
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['dec-photography.herokuapp.com', 'localhost']
 
