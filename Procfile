c6360ccda62318d92009fbc869fe06f7b21e4181
dimitar-4 <dcholakov4@gmail.com>
2021-03-21 17:46:10 +0000

    Add Procfile, set Allowed hosts and database urls in settings

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn dec_photography.wsgi:application
--- dec_photography/settings.py
+++ dec_photography/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = '*5al*9n$hc0&nz+&et^@681racqq9zwsg#$es49v6d=djwp8xx'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['dec-photography.herokuapp.com', 'localhost']
 
 
 # Application definition
@@ -122,12 +122,17 @@ WSGI_APPLICATION = 'dec_photography.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+if 'DATABASE_URL' in os.environ:
+    DATABASES = {
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
 
 
 # Password validation
